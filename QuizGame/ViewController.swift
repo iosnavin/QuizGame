7743a8914f8533e756b76f70de2d6f466967d2d6
Naveen <ios.nreddy@gmail.com>
2022-09-01 18:23:38 +0530

    Initial Commit

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Naveen Reddy on 01/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
